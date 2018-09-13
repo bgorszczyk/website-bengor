647698f0fae0e5b48794301064bf3def77d811d9
Bengor <Bengor@DESKTOP-3646U54>
2018-09-13 14:42:07 +0200

    Premier commit

---
+++ src/main/java/com/bengor/website/websitebengor/WebsiteBengorApplication.java
@@ -0,0 +1,12 @@
+package com.bengor.website.websitebengor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsiteBengorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsiteBengorApplication.class, args);
+	}
+}
---
+++ src/test/java/com/bengor/website/websitebengor/WebsiteBengorApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bengor.website.websitebengor;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebsiteBengorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
