b5115eec8c910905f67fe8513703d22e899b2389
Bengor <Bengor@DESKTOP-3646U54>
2018-09-17 13:32:48 +0200

    Ajout de test JUnit pour la class BienvenueController

---
+++ README
@@ -0,0 +1,1 @@
+[![Build Status](https://travis-ci.com/bgorszczyk/website-bengor.svg?branch=prod)](https://travis-ci.com/bgorszczyk/website-bengor)
---
+++ src/test/java/com/bengor/website/websitebengor/BienvenueControllerTest.java
@@ -0,0 +1,36 @@
+package com.bengor.website.websitebengor;
+
+import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(BienvenueController.class)
+public class BienvenueControllerTest {
+
+    private MockMvc mockMvc;    
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Before
+    public void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+    
+	@Test
+	public void test() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/")).andExpect(status().isOk()).andExpect(view().name("bienvenue"));
+	}
+
+}
